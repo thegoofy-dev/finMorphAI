2e249666dd0a74205955c3c806018c6f341bd430
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-07-12 17:14:51 +0530

    Update Header component to check user authentication on load & storing user data in DB
    - Modified Header component to be asynchronous and call checkUser function on render
    - Ensured user authentication is verified before rendering the header

--- components/Header.jsx
+++ components/Header.jsx
@@ -11,8 +11,10 @@ import React from "react";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
 import Link from "next/link";
+import { checkUser } from "@/lib/checkUser";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
