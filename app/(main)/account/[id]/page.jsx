92ee2d6d9729db4a6cba7c205896d192e739444b
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-07-13 17:32:44 +0530

    feat: add account retrieval with transactions to AccountPage
    - Implemented getAccountWithTransactions function to fetch account details along with associated transactions.
    - Created AccountPage component to display account information and transaction count.
    - Added error handling for unauthorized access and missing accounts.

---
+++ app/(main)/account/[id]/page.jsx
@@ -0,0 +1,41 @@
+import { getAccountWithTransactions } from "@/actions/accounts";
+import { notFound } from "next/navigation";
+
+const AccountPage = async ({ params }) => {
+  const accountsData = await getAccountWithTransactions(params.id);
+  console.log(accountsData);
+  if (!accountsData) {
+    notFound();
+  }
+
+  const { transactions, ...account } = accountsData;
+
+  return (
+    <div className="space-y-8 px-5 flex gap-4 items-center justify-between">
+      <div>
+        <h1 className="text-5xl sm:text-6xl font-bold bg-gradient-to-br from-blue-600 to-purple-600 pr-2 pb-2 text-transparent bg-clip-text capitalize">
+          {account.name}
+        </h1>
+        <p className="text-muted-foreground">
+          {account.type.charAt(0).toUpperCase() +
+            account.type.slice(1).toLowerCase()}{" "}
+          Account
+        </p>
+      </div>
+      <div className="text-right pb-2">
+        <div className="text-xl sm:text-2xl font-bold">
+          ${parseFloat(account.balance).toFixed(2)}
+        </div>
+        <p className="text-sm text-muted-foreground">
+          {account._count.transactions} Transactions
+        </p>
+      </div>
+
+      {/* Chart Section */}
+
+      {/* Transaction Table */}
+    </div>
+  );
+};
+
+export default AccountPage;
