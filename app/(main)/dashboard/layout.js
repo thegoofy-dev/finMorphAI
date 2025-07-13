ec45871a7f059a5c768698e2f7272e866b0329e0
Pankaj Tyagi <itstyagi0001@gmail.com>
2025-07-13 06:47:11 +0530

    Update Dashboard component and add react-spinners dependency
    - Renamed Dashboard component to DashboardPage for clarity
    - Enhanced layout with additional styling
    - Added react-spinners package to package.json and package-lock.json

---
+++ app/(main)/dashboard/layout.js
@@ -0,0 +1,19 @@
+import React, { Suspense } from 'react'
+import DashboardPage from './page';
+import { BarLoader } from "react-spinners";
+
+const DashboardLayout = () => {
+    return (
+        <div className="px-5">
+            <h1 className="text-6xl font-bold bg-gradient-to-br from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4">
+                Dashboard
+            </h1>
+            {/* Dashboard Page */}
+            <Suspense fallback={<BarLoader className='mt-4' width={"100%"} color='#9333ea'/>}>
+                <DashboardPage />
+            </Suspense>
+        </div>
+    );
+}
+
+export default DashboardLayout
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-const Dashboard = () => {
+const DashboardPage = () => {
   return (
-    <div >
-      DashBoard Page
+    <div className="px-5">
+      <h1 className="text-6xl font-bold bg-gradient-to-br from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4">
+        {/* Budget Progress */}
+
+        {/* Overview */}
+
+        {/* Accounts Grid */}
+      </h1>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default DashboardPage
